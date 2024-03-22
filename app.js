901a90827772048fd101c42fb78b0f796d7bf781
RadioactiveGhost <radioactiveghost96@gmail.com>
2024-03-22 12:42:26 +0000

    Restruturação de código Transição para express-handlebars

--- app.js
+++ app.js
@@ -1,56 +1,38 @@
 //* Implementação dos Módulos do NodeJS ---
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
 const express = require('express');
-const app = module.exports.app = express();
-const engines = require('consolidate');
-const ejs = require('ejs');
-let server = http.createServer(app);
+const http = require('http');
 const url = require('url');
-const bodyParser = require('body-parser');
-const io = require('socket.io')(server);
-let os = require('os');
-require('dotenv-safe').config();
-session = require('express-session')({
-    secret: 'mysecret',
-    resave: true,
-    saveUninitialized: true
-});
-sharedsession = require("express-socket.io-session");
-io.use(sharedsession(session));
-app.use(session);
+const path = require('path');
+const { engine } = require('express-handlebars');
+const { equipamentoLista } = require('./server/variables.js')
 const SSHClient = require('ssh2').Client;
 /* ------------------------------------- */
 
 //* Variáveis -----------------------------
-const { list } = require('./server/deets');
+const port = 8888;
+const app = module.exports.app = express();
 const log = console.log;
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 let sshost = '';
 let ssport = '';
 let ssusername = '';
 let sspassword = '';
-    //* Porta a aceder ao website
-const serverPort = 8888;
 /* ------------------------------------- */
 
 /*
 * Declarações para o express.js -----------
 *Tratam de encaminhamento de ficheiros ----
 */
-app.use("/", express.static(__dirname + "/public"));
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', engine({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}));
 app.use("/modules", express.static(__dirname + "/node_modules"));
-app.set('views', __dirname + '/public/');
-app.engine('html', engines.ejs);
-app.set('view engine', 'html');
-app.use(express.json());
-//? Verificar se necessário
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.static('public'));
 /* ------------------------------------- */
 
 //* Conexão SSH ---------------------------
@@ -78,77 +60,79 @@ function sshConnection(socket) {
     }).on('error', function (err) {
         console.log('Error: ' + err.message);
         socket.emit('data', '\r\n- Erro de Conexão: ' + err.message + '/!\\\r\n');
-    })/*.on('disconnect', function () {
-        console.log('Disconnected');
-    })*/;
+    })//.on('disconnect', function () {
+        //console.log('Disconnected');
+    //})
     console.log('A user connected');
     socket.on('disconnect', function () {
         console.log('A user disconnected');
         io.off('connection', ioConnection);
         conn.off('disconnect', sshConnection);
         //? Potencialmente desnecessário
-        /*socket.disconnect();
-        socket.removeAllListeners();*/
+        //socket.disconnect();
+        //socket.removeAllListeners();
     });
     conn.connect({
-        host: sshost,
-        port: ssport,
-        username: ssusername,
-        password: sspassword
+        host: '10.200.1.20',
+        port: '22',
+        username: 'adr4k',
+        password: 'adr4k'
     });
 };
 
 function ioConnection(socket) {
     sshConnection(socket);
-};
+}
 /* ---------------------------------- */
 
-//* Encaminhamento de endereços -----------
-app.get('/home', (req, res) => {
-    log("Teste");
+//* 
+
+function listConnection(socket)  {
     io.on('connection', function(socket) {
         socket.emit('hello', 'wassup');
         socket.emit('gerar', list);
         socket.disconnect();
         socket.removeAllListeners();
     }).off('connection', () => log("Desligado"));
-    res.render('index.html');
-});
+    socket.on('disconnect', function () {
+        log('Index disconnected');
+        io.off('connection', lsConnection);
+    });
+}
 
-app.post('/test', (req, res) => {
-    let name = req.body.name;
-    res.render('test.html', { name: name });
-});
+function lsConnection(socket) {
+    listConnection(socket);
+}
 
-app.get('/terminal', (req, res) => {
+/* ---------------------------------- */
+
+//* Encaminhamento de endereços -----------
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: 'main', equipamentoLista: equipamentoLista, listExists: true});
+});
+/*app.get('/terminal', (req, res) => {
     io.on('connection', ioConnection);
-    res.render('terminal.html');
+    res.render('terminal');
+})*/
+app.get('/about', (req, res) => {
+    res.render('about', {layout: 'main'});
 });
-app.post('/linux', (req, res) => {
-    sshost = process.env.CON1HOST;
-    ssport = process.env.CON1PORT;
-    ssusername = process.env.CON1USER;
-    sspassword = process.env.CON1PASS;
-
+app.post('/equipamento_:id', (req, res) => {
+    log ('Selected Equipment: ' + req.params.id);
+    for (let i = 0; i < equipmentList.length; i++) {
+        if (req.params.id === equipmentList[i].pk) {
+            log('Type: ' + equipmentList[i].host);
+            sshost = equipmentList[i].host;
+            ssport = equipmentList[i].port;
+            ssusername = equipmentList[i].user;
+            sspassword = equipmentList[i].pass;
+            break;
+        }
+    }
     io.on('connection', ioConnection);
-    res.render('terminal.html');
-});
-
-app.post('/android', (req, res) => {
-    sshost = process.env.CON2HOST;
-    ssport = process.env.CON2PORT;
-    ssusername = process.env.CON2USER;
-    sspassword = process.env.CON2PASS;
-
-    io.on('connection', ioConnection);
-    res.render('terminal.html');
-});
-
-//TODO Better fetching of urls (and maybe more secure?)
-app.post('/something:id', (req, res) => {
-    log('success: ' + req.params.id + "\nFrom deets: " + list[0][0]);
-    res.end();
-});
+    res.render('terminal');
+})
 
 /*app.get('*', function (req, res) {
     res.redirect('/');
@@ -156,6 +140,6 @@ app.post('/something:id', (req, res) => {
 /* --------------------------------------*/
 
 //* Iniciar o Server ----------------------
-server.listen(serverPort, () => {
-    log("Serviço iniciado em :" + serverPort)
+server.listen(port, () => {
+    console.log(`App listening to port ${port}`);
 });
